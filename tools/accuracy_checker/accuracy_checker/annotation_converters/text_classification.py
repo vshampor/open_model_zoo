cac2805539022636dc8e7d91d8524c86a5f0fecd
Vasily Shamporov <vasily.shamporov@intel.com>
2020-04-09 14:29:59 +0300

    Use 0 for [CLS] segment ID in XNLI

--- tools/accuracy_checker/accuracy_checker/annotation_converters/text_classification.py
+++ tools/accuracy_checker/accuracy_checker/annotation_converters/text_classification.py
@@ -95,7 +95,7 @@ class BaseGLUETextClassificationConverter(BaseFormatConverter):
         segment_ids = []
         if self.class_token_first:
             tokens.append("[CLS]" if self.support_vocab else CLS_ID)
-            segment_ids.append(SEG_ID_CLS)
+            segment_ids.append(SEG_ID_A)
         for token in tokens_a:
             tokens.append(token)
             segment_ids.append(SEG_ID_A)
